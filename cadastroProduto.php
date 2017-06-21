af671c425bc48d27c48c9268abb78c25474e4e28
Maiky <maiky@MacBook-Pro-de-Maiky.local>
2017-06-20 22:01:41 -0300

    ajustes cadastro de produto e criado processa produto

--- cadastroProduto.php
+++ cadastroProduto.php
@@ -6,45 +6,31 @@
 </head>
 <body>
   
- <div style="margin-right: auto; margin-left: auto; width: 165px">
+	<div style="margin-right: auto; margin-left: auto; width: 165px">
 		<h3 align="center">Produto:</h3>
+	
+		<form action="processaProduto.php" name="frmCadastroProduto" method="post">
+			<input type="text"   name="cod" placeholder="C贸digo do Produto"            required="required" style="width: 170px" autofocus="autofocus"/><p>
+			<input type="text"   name="nom" placeholder="Nome do Produto"              required="required" style="width: 170px" maxlength="20"/><p>
+			<input type="text"   name="fab" placeholder="Nome do Fabricante"           required="required" style="width: 170px" /><p>
+			<input type="number" name="com" placeholder="Valor de Compra"              required="required" style="width: 170px" step='any'><p>
+			<input type="number" name="ven" placeholder="Valor de Venda"               required="required" style="width: 170px" step='any' /><p>
+			<input type="text"   name="des" placeholder="Percentual de desconto"       required="required" style="width: 170px" /><p>
+			<input type="text"   name="esp" placeholder="Especifica莽茫o"                required="required" style="width: 170px" maxlength="20" /><p>
+			<input type="text"   name="qua" placeholder="Quantidade de compra"         required="required" style="width: 170px" /><p>
 
-		<form action="" name="frmcadastroproduto" method="post">
-		<input type="hidden" name="op" value="I" /> 
-
-		
-		<input type="text"name="codpro" placeholder="C骴igo do Produto" required="required"autofocus="autofocus"style="width: 170px/>
-		<p />
-		<input type="text" name="nompro" placeholder="Nome do Produto" required="required" maxlength="20" style="width: 170px/>
-		<p />
-		<input type="text" name="fabpro" required="required"placeholder="Nome do Fabricante do Produto"autofocus="autofocus"style="width: 170px" />
-		<p />
-		<input type="number" step='any'name="valcompro" placeholder="Valor de Compra do Produto" required="required"autofocus="autofocus"style="width: 170px/>
-		<p />
-		<input type="number" step='any'name="valvenpro" placeholder="Valor de Venda do Produto" required="required"autofocus="autofocus" style="width: 170px/>
-		<p />
-		<input type="text"name="perdespro" placeholder="Percentual de desconto do Produto" required="required"autofocus="autofocus"style="width: 170px/>
-		<p />
-		<input type="text"name="esppro" placeholder="Especifica玢o do Produto" required="required"autofocus="autofocus" maxlenght="20" style="width: 170px/>
-		<p />
-		<input type="text"name="quapro" placeholder="Quantidade de compra do produto" required="required"autofocus="autofocus"style="width: 170px/>
-		<p />
-		<input type="date"name="datcadpro" placeholder="Digite a data de cadastro no sistema" required="required"autofocus="autofocus"style="width: 170px/>
-		<p />
-		<input type="text"name="codtabdes" placeholder="C骴igo da tabela de desconto" required="required"autofocus="autofocus" style="width: 170px/>
-		<p />
-		<input type="radio"  name= "codcat" value ="A" /> Antibi髏icos &nbsp;&nbsp; <p/> 
-		<input type="radio"  name= "codcat" value ="C" /> Controlados &nbsp;&nbsp; <p/>
-		<input type="radio"  name= "codcat" value ="H" /> Higiene &nbsp;&nbsp; <p/>
-		<input type="radio"  name= "codcat" value ="O" /> Outros &nbsp;&nbsp; <p/>   
-		<input type="radio"  name= "codcat" value ="G" /> Gen閞ico <p />
-		
-		<input type="text"name="codtabpre" placeholder="C骴igo da tabela de pre鏾" required="required"autofocus="autofocus"style="width: 170px/>
-		
-			<p />
-			<p />
+			<input type="text"   name="tbd" placeholder="C贸digo da tabela de desconto" required="required" style="width: 170px" autofocus="autofocus" />		<p>
+			<input type="text"   name="tbp" placeholder="C贸digo da tabela de pre莽o"    required="required" style="width: 170px/"><p>
+			
+			<input type="radio"  name="cat" value ="A" /> Antibi贸ticos &nbsp;&nbsp; <p> 
+			<input type="radio"  name="cat" value ="C" /> Controlados &nbsp;&nbsp; <p>
+			<input type="radio"  name="cat" value ="H" /> Higiene &nbsp;&nbsp; <p>
+			<input type="radio"  name="cat" value ="O" /> Outros &nbsp;&nbsp; <p>   
+			<input type="radio"  name="cat" value ="G" /> Gen茅rico <p><p>
+			
 			<button name="btnsalvar" type="submit" style="width: 170px">Salvar</button>
 			<button name="btnlimpar" type="reset" style="width: 170px">Limpar</button>
 		</form>
+	</div>
 </body>
 </html>
