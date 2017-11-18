701d697f15356317078ec9e7c9ab83885280bea6
JonathanDalpiaz <dalpiazj@gmail.com>
2017-11-18 14:23:00 -0200

    criação das views

--- index.php
+++ index.php
@@ -54,11 +54,7 @@ require 'sessao.php';
 	
 	function emitirRelatorio1(){
 		global $conexao;
-		$sql = 'SELECT t.cgctrs, t.nomtrs, t.datcadtrs FROM transacionador t
-                INNER JOIN tipo_transacionador p ON t.codtiptrs = p.codtiptrs
-				WHERE t.sextrs = \'F\' AND
-					  p.tiptrs = \'C\'
-				ORDER BY nomtrs DESC';
+		$sql = 'SELECT t.cgctrs, t.nomtrs, t.datcadtrs FROM VW_RELATORIO1 t';
 		
 		$sql = $conexao->prepare($sql);
 		$sql->execute();
@@ -84,12 +80,7 @@ require 'sessao.php';
 	
 	function emitirRelatorio2(){
 		global $conexao;
-		$sql = 'SELECT p.codpro, p.nompro, p.valvenpro FROM produto p
-				INNER JOIN categoria c ON p.codcat = c.codcat
-				WHERE extract(YEAR FROM datcadpro) = 2017 AND
-					  valvenpro > 100 AND
-					  c.codcat = 6
-				ORDER BY nompro ASC';
+		$sql = 'SELECT p.codpro, p.nompro, p.valvenpro FROM VW_RELATORIO2 p';
 		
 		$sql = $conexao->prepare($sql);
 		$sql->execute();
@@ -116,13 +107,8 @@ require 'sessao.php';
 	function emitirRelatorio3(){
 		global $conexao;
 		
-		$sql = 'SELECT EXTRACT(MONTH FROM v.datven) AS mes, count(v.*) AS qtd, sum(valtotven) as val FROM venda v
-				INNER JOIN venda_item i ON i.codven = v.codven
-				INNER JOIN transacionador t ON t.cgctrs = v.cgctrs
-				WHERE i.valdesitm > 0 AND
-					  extract(YEAR FROM v.datven) = 2016 AND
-					  sextrs = \'M\'
-				GROUP BY mes';
+		$sql = 'SELECT EXTRACT(MONTH FROM v.datven) AS mes, count(v.*) AS qtd, sum(valtotven) as val FROM VW_RELATORIO3 v';
+				
 		
 		$sql = $conexao->prepare($sql);
 		$sql->execute();
@@ -149,12 +135,7 @@ require 'sessao.php';
 	
 	function emitirRelatorio4(){
 		global $conexao;
-		$sql = 'SELECT t.cgctrs, v.codven, v.datven, v.valtotven FROM venda v
-				INNER JOIN transacionador t ON t.cgctrs = v.cgctrs
-				WHERE (upper(t.cidtrs) like (\'MARAVILHA\') OR
-					   upper(t.cidtrs) like (\'DESCANÇO\')) AND
-					   mod(cast(EXTRACT(MONTH FROM v.datven)as integer), 2) > 0
-				ORDER BY datven DESC';
+		$sql = 'SELECT t.cgctrs, v.codven, v.datven, v.valtotven FROM VW_RELATORIO4 v';
 		
 		$sql = $conexao->prepare($sql);
 		$sql->execute();
